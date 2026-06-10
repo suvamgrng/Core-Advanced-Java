2aab6b7142aa149dbfa032eb11f740fafb09baf7
suvamgurung <gurungsuvam265@gmail.com>
2026-06-10 21:36:24 +0545

    Initial Commit

--- src/main/java/RunApp/App.java
+++ src/main/java/RunApp/App.java
@@ -1,12 +1,14 @@
 package RunApp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan("RunApp")
+//@Configuration
+//@ComponentScan("RunApp")
+@SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
